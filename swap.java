cd62c891cdbadc1eef2265e9ca1d01486c96f73e
Shibani Gupta <shibanigupta1170@gmail.com>
2026-06-01 23:58:54 +0530

    Implement swap functionality in swap.java
    Swapped two variables with using the third variable.

---
+++ swap.java
@@ -0,0 +1,13 @@
+class swap
+{
+  public static void main(String args[])
+    {
+        int a=45;
+        int b=56;
+        int c=0;
+        c=a;
+        a=b;
+        b=c;
+        System.out.println("The numbers after swapping is:",a,b);
+    }
+}
